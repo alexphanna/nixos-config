06258516efaec06db9f6e4c09aff8f0106e4d487
alexphanna <ahanna0608@gmail.com>
2024-09-02 13:34:37 -0400

    Add laptop xinitrc

--- modules/home/dot-files/dot-files.nix
+++ modules/home/dot-files/dot-files.nix
@@ -1,7 +1,7 @@
 { host, ... }:
 {
     home.file = {
-        ".xinitrc".source = if host == "desktop" then ./xinitrc.desktop else ./xinitrc.laptop;
+        ".xinitrc".source = if host == "desktop" then ./desktop.xinitrc else ./laptop.xinitrc;
         ".wallpaper.jpg".source = ./wallpaper.jpg;
     };
 }
