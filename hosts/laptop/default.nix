87411594f7713a66e823d31d7a4d2fb3e0ec210d
alexphanna <ahanna0608@gmail.com>
2025-01-26 15:04:44 -0500

    Update vscode

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -9,13 +9,14 @@
 
   environment.systemPackages = with pkgs; [
     acpilight
+    powertop
   ];
 
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
   '';
 
-  services.auto-cpufreq.enable = true;
+  /*services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
       governor = "powersave";
@@ -24,6 +25,16 @@
     charger = {
       governor = "performance";
       turbo = "auto";
+    };
+  };*/
+  
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 20;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT1 = 20;
+      STOP_CHARGE_THRESH_BAT1 = 80;
     };
   };
 }
--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -2,17 +2,20 @@
 {
   programs.vscode = {
     enable = true;
+    # package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       vscjava.vscode-java-pack
       bbenoist.nix
       ms-python.python
-      ms-dotnettools.csdevkit
+      ms-vscode-remote.remote-ssh
     ];
 
     userSettings = {
       "java.jdt.ls.java.home" = "${pkgs.jdk23}";
       "workbench.colorTheme" = "Default Dark+";
       "files.autoSave" = "afterDelay";
+      "workbench.statusBar.visible" = false;
+      "editor.minimap.enabled" = false;
     };
   };
 
